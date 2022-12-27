ea7ce5f5a130f448802095ba23fd586287c09f1d
Victor Monte <victorbcm@hotmail.com>
2022-12-27 19:57:00 +0000

    Add invicibility and flashing

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -16,6 +16,14 @@ public class PlayerHealthController : MonoBehaviour
     public int currentHealth;
     public int maxHealth;
 
+    public float invicibilityLength;
+    private float invicCounter;
+
+    public float flashLength;
+    private float flashCounter;
+
+    public SpriteRenderer[] playerSprites;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,20 +35,50 @@ public class PlayerHealthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (invicCounter > 0)
+        {
+            invicCounter -= Time.deltaTime;
+
+            flashCounter -= Time.deltaTime;
+            if (flashCounter <= 0)
+            {
+                foreach (var sprite in playerSprites)
+                {
+                    sprite.enabled = !sprite.enabled;
+                }
+                flashCounter = flashLength;
+            }
+
+            // Fix visibility bug
+            if (invicCounter <= 0)
+            {
+                foreach (var sprite in playerSprites)
+                {
+                    sprite.enabled = true;
+                }
+                flashCounter = 0;
+            }
+        }
     }
 
     public void DamagePlayer(int damageAmount)
     {
-        currentHealth -= damageAmount;
-
-        if (currentHealth <= 0)
+        if (invicCounter <= 0)
         {
-            currentHealth = 0;
+            currentHealth -= damageAmount;
 
-            gameObject.SetActive(false);
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                invicCounter = invicibilityLength;
+            }
+
+            UIController.instance.UpdateHealth(currentHealth, maxHealth);
         }
-
-        UIController.instance.UpdateHealth(currentHealth, maxHealth);
     }
 }
